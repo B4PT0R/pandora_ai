14f3fca46efeacfbc8e579978bfbdf2309d9646d
B4PT0R <bferrand.maths@gmail.com>
2024-01-28 15:21:55 +0100

    //

--- pandora_ai/__init__.py
+++ pandora_ai/__init__.py
@@ -1,4 +1,3 @@
-from .pandora_agent import Pandora,Tool,Message,Image, NoContext
+from .pandora_agent import Pandora,Tool,Message,Image,NoContext
 from .console import Console
 
-Pandora.setup_folder()
